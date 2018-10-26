176f7201c602d20d95dd47312c2974d809712784
Rolf Staflin <gitlab@piro.se>
2018-10-26 21:53:19 +0200

    Day02: reading the input file

--- Day02.go
+++ Day02.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
 	Day02()
 }
 
 func Day02() {
-
+	bytes, err := ioutil.ReadFile("input/Day02.txt")
+	if err != nil {
+		panic(err)
+	}
+	var input = string(bytes)
+	fmt.Print(input)
 }
