13290e984c182285e87866850b9c4af48e792605
Rolf Staflin <gitlab@piro.se>
2018-11-07 17:01:15 +0100

    Day02 is now using goroutines

--- Day02.go
+++ Day02.go
@@ -6,11 +6,19 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
 	Day02()
 }
+
+var (
+	mutex        sync.Mutex
+	part1, part2 int
+)
+
+const numberOfThreads = 6
 
 func Day02() {
 	bytes, err := ioutil.ReadFile("input/Day02.txt")
@@ -18,28 +26,47 @@ func Day02() {
 		panic(err)
 	}
 	var input = string(bytes)
-	var lines = strings.Split(input, "\n")
+	var lines = strings.Split(input, "\r\n")
 
-	part1 := 0
-	part2 := 0
-	for i, line := range lines {
-		var dimensionStr = strings.Split(line, "x")
-		var dimensions = mapToSortedInts(dimensionStr)
-
-		// Paper size
-		size := 3 * dimensions[0] * dimensions[1]
-		size += 2 * dimensions[0] * dimensions[2]
-		size += 2 * dimensions[1] * dimensions[2]
-		part1 += size
-
-		// Ribbon
-		volume := dimensions[0] * dimensions[1] * dimensions[2]
-		ribbon := 2*(dimensions[0]+dimensions[1]) + volume
-		part2 += ribbon
-		fmt.Println(i, dimensions, size, ribbon)
+	lineChannel := make(chan string, numberOfThreads)
+	doneChannel := make(chan int, numberOfThreads)
+	for n := 0; n < numberOfThreads; n++ {
+		go calculate(lineChannel, doneChannel)
 	}
+
+	for _, line := range lines {
+		lineChannel <- line
+	}
+	close(lineChannel)
+
+	for i := 0; i < numberOfThreads; i++ {
+		<-doneChannel
+	}
+
 	fmt.Println("Part 1", part1)
 	fmt.Println("Part 2", part2)
+}
+
+func calculate(lineChannel chan string, doneChannel chan int) {
+
+	for line := range lineChannel {
+		var dimensionStr = strings.Split(line, "x")
+		var dimensions = mapToSortedInts(dimensionStr)
+		// Paper size
+		paperSize := 3 * dimensions[0] * dimensions[1]
+		paperSize += 2 * dimensions[0] * dimensions[2]
+		paperSize += 2 * dimensions[1] * dimensions[2]
+		// Ribbon
+		volume := dimensions[0] * dimensions[1] * dimensions[2]
+		ribbonLength := 2*(dimensions[0]+dimensions[1]) + volume
+
+		// fmt.Println(line, dimensions, paperSize, ribbonLength)
+		mutex.Lock()
+		part1 += paperSize
+		part2 += ribbonLength
+		mutex.Unlock()
+	}
+	doneChannel <- 42
 }
 
 func mapToSortedInts(strs []string) []int {
